6388b68dc1ca7e9b42b83ab4a838384d3cf4bfe9
Мелася <poliuha.pn@ucu.edu.ua>
2026-03-27 18:36:44 +0200

    Add solution for Linked Lists - Move Node

---
+++ Linked_Lists_Move_Node/#5 Move Node.py
@@ -0,0 +1,19 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+def move_node(source, dest):
+    if source is None:
+        raise Exception("Source list is empty")
+
+    first_node = source
+    source = source.next
+    first_node.next = dest
+    dest = first_node
+    return Context(source, dest)
